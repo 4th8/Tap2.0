4c20d365b6c98bb11a8ab697ef466b66ecf0b165
Nathan <nwoodhea@mail.umw.edu>
2016-10-16 22:11:34 -0400

    Added Javadoc

--- TAP2.0/src/tap2/pkg0/ImportDataFile.java
+++ TAP2.0/src/tap2/pkg0/ImportDataFile.java
@@ -1,8 +1,5 @@
 /*
- * Reads in the .csv file and adds the data to the database.
- * Will Automaticly find the S/N of the Sensor and use that to queary the
- * database to find the location of that sensor. If no location exists will queary
- * the user to input the location and update the database with that location / SN combo
+ 
  */
 package tap2.pkg0;
 import java.io.IOException;
@@ -12,17 +9,31 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
- *
- * @author i4th8
+ * Reads in the .csv file and adds the data to the database.
+ * Will automatically find the S/N of the Sensor and use that to query the
+ * database to find the location of that sensor. If no location exists will queary
+ * the user to input the location and update the database with that location / SN combo
+ * @author Nathan
+ * 
  */
 public class ImportDataFile {
     private final String filename; //Filename for the input file
     private final dbQuery db;
+    
+    /**
+     * Default Constructor
+     * @param filename Filename of the .xlsx file.
+     * @param db dbQueary object. An interface with the database.
+     */
     ImportDataFile(String filename, dbQuery db){
         this.filename = filename;
         this.db = db;
         execute();
     }
+    /**
+     * Does most of the work for Importing the file into the database.
+     * @author Nathan
+     */
     private void execute(){
         try {
             XSSFWorkbook workbook = new XSSFWorkbook(filename);
