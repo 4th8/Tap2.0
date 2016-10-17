d18f446049ddc211e38e82e77dfac71465bf23be
Nathan <nwoodhea@mail.umw.edu>
2016-10-16 21:15:35 -0400

    more stuff

--- TAP2.0/src/tap2/pkg0/ImportDataFile.java
+++ TAP2.0/src/tap2/pkg0/ImportDataFile.java
@@ -1,14 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Reads in the .csv file and adds the data to the database.
+ * Will Automaticly find the S/N of the Sensor and use that to queary the
+ * database to find the location of that sensor. If no location exists will queary
+ * the user to input the location and update the database with that location / SN combo
  */
 package tap2.pkg0;
+import org.apache.poi.hssf.eventusermodel;
 
 /**
  *
  * @author i4th8
  */
 public class ImportDataFile {
+    private final String filename; //Filename for the input file
+    private final dbQuery db;
+    ImportDataFile(String filename, dbQuery db){
+        this.filename = filename;
+        this.db = db;
+        execute();
+    }
+    private void execute(){
+        XSSFWorkbook workbook = new XSSFWorkbook(filename);
+}
     
 }
