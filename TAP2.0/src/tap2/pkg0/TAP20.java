a5e68bf98aff060f16d246ea0717fabc6053353b
woodb <woodb@10.35.115.200>
2016-10-21 03:56:00 -0400

    Nathan

--- TAP2.0/src/tap2/pkg0/TAP20.java
+++ TAP2.0/src/tap2/pkg0/TAP20.java
@@ -5,10 +5,12 @@
  */
 package tap2.pkg0;
 
+import GUI.dataPage;
 import java.sql.SQLException;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
 
 /**
  *
@@ -20,20 +22,11 @@ public class TAP20 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        String see;
-        db dataBase = new db();
-        dbQuery query = new dbQuery();
-        try {
-            ResultSet rs =dataBase.executeSelect(query.getLocationIdBySerialNumber("k"));
-            rs.next();
-            //dataBase.executeInsert(query.insertTemperatureData("1998-01-08 04:05:06", 5,"k","000" ));
-            //dataBase.executeInsert(query.insertSensor("k", "D"));
-            System.out.println(rs.getString(1));
-            //db.connectToDB("Select * from location;");
-        } catch (SQLException ex) {
-            Logger.getLogger(TAP20.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        db database = new db();
+        dbQuery query = new dbQuery(database);
+        dataPage frame = new dataPage(query);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
-
 }
